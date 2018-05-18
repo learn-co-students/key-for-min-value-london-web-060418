0320d029ef14146b212dc3196965493b1dd1584e
Jigar Polra <jigarpolra@live.co.uk>
2018-05-18 13:41:53 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,18 @@
 
 def key_for_min_value(name_hash)
 
+  if name_hash.length == 0 then
+    return nil
+  else
+  min_hash = { name: "", value: 10000000 }
+
+  name_hash.each do |key, value|
+    if(value < min_hash[:value])
+      min_hash[:name] = key
+      min_hash[:value] = value
+    end
+  end
+
+  return min_hash[:name]
+  end
 end
